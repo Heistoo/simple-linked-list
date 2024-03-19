49a8ce432be49b73e26dd6d6ef68d580d26454dd
Heistoo <46348338+Heistoo@users.noreply.github.com>
2024-03-19 11:09:03 -0300

    feature: to do list

--- TesteLista.java
+++ TesteLista.java
@@ -14,12 +14,16 @@ public class TesteLista{
             l.insereFim(i*10+5);
             System.out.println(l);
         }
-        if (l.estaVazia()){
-            System.out.println("Lista vázia, não há o que remover");
-        }
-        else{
-            System.out.println(l.removeInicio()+ " saiu do inicio");
-        }
+        // if (l.estaVazia()){
+        //     System.out.println("Lista vázia, não há o que remover");
+        // }
+        // else{
+        //     System.out.println(l.removeFim()+ " saiu do fim");
+        // }
 
+        //DIVERSÃO!!!
+        //1- IMPLEMENTAR O removeFim para LISTA
+        //2- implementar o removeFim para LISTACOMULTIMO
+        //3- IMPLEMENTAR UM TESTE PARA LISTACOMULTIMO
     }
 }
