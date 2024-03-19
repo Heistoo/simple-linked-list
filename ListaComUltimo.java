fa3323a11588b9f2074f3099b1075f847472ab8c
Heistoo <46348338+Heistoo@users.noreply.github.com>
2024-03-19 11:03:20 -0300

    modified code for better implementation

--- ListaComUltimo.java
+++ ListaComUltimo.java
@@ -15,15 +15,15 @@ public class ListaComUltimo extends Lista{
         }
         super.setPrimeiro(novo);
     }
+    @Override
     public void insereFim(int i){
         No novo = new No(i);
         if (estaVazia()){
-            ultimo = novo;
             super.setPrimeiro(novo);
         }
         else{
-            ultimo.setProximo(novo); //proximo do ultimo vai ser o novo
-            ultimo = novo; //a lista vai reconhecer um novo elemento como o ultimo, e o elemento anterior que era o "ultimo" agora será o penultimo
-        }
+            ultimo.setProximo(novo); //proximo do ultimo vai ser o "novo" elemento   
+        } 
+        ultimo = novo; //a lista vai reconhecer um novo elemento como o ultimo, e o elemento anterior que era o "ultimo" agora será o penultimo
     }
 }
